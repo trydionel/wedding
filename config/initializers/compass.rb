6c701d93e1b21893a34f356ad69c07c167d8309a
Jeff Tucker <trydionel@gmail.com>
2009-10-17 14:03:07 -0400

    Setting up compass for dev only

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,7 @@
-require 'compass'
-# If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
-Compass.configure_sass_plugin!
+if 'development' == RAILS_ENV
+  require 'compass'
+  # If you have any compass plugins, require them here.
+  Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+  Compass.configuration.environment = RAILS_ENV.to_sym
+  Compass.configure_sass_plugin!
+end
