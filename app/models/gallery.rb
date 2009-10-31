0b8afd9381a2b6548b1c69fc59fbfae4f9610540
Jeff Tucker <trydionel@gmail.com>
2009-10-31 19:32:30 -0400

    Fixing bug in galleries

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,12 @@
 class Gallery < ActiveRecord::Base
   has_many :photos
   has_many :comments, :as => :commentable
+  
+  def preview_thumbnail_url
+    unless photos.empty?
+      photos.first.image.url(:thumbnail)
+    else
+      "public/images/missing.png"
+    end
+  end
 end
