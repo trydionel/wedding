daf0741bdf9c49a3a2933c02d61660a572b90ea6
Jeff Tucker <trydionel@gmail.com>
2009-11-08 19:17:32 -0500

    Fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.create(:username => "jeff", :password => "trydionel", :password_confirmation => "trydionel")
-User.create(:username => "alayna", :password => "thymebombe", :password_confirmation => "thymebombe")
+User.create(:username => "jeff", :email => "trydionel@gmail.com", :password => "trydionel", :password_confirmation => "trydionel")
+User.create(:username => "alayna", :email => "thymebombe@yahoo.com", :password => "thymebombe", :password_confirmation => "thymebombe")
 
 Gallery.create(:title => "Engagement", :description => "Photos during Jeff & Alayna's engagement period")
 Gallery.create(:title => "Wedding", :description => "Photos during Jeff & Alayna's wedding")
