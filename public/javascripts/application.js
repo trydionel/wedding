8d7a18df56612332b1a854b29dbc05e3fbb1c86d
Jeff Tucker <trydionel@gmail.com>
2009-12-05 16:47:01 -0500

    Adding asynch GA code, improving some page layout, adding Delete comment link

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,23 +1,35 @@
-document.observe('dom:loaded', function() {
-  var pathname = function(url) {
-    return url.replace(location.protocol + '//' + location.host, '');
-  }
-  
-  // Activate nav buttons based on location
-  $('nav').select('a').each(function(element) {
-    var path = pathname(element.href)
-    var len = path.length;
+var pathname = function(url) {
+  return url.replace(location.protocol + '//' + location.host, '');
+}
+
+var highlightCurrentLink = function(element) {
+	var path = pathname(element.href)
+	var len = path.length;
     
-    element.removeClassName('current');
-    if (location.pathname == path) {
-      element.addClassName('current');
-    } else if (len > 1 && location.pathname.substring(0, len) == path) {
-      element.addClassName('current');
-    }
+	element.removeClassName('current');
+	if (location.pathname == path) {
+	  element.addClassName('current');
+	} else if (len > 1 && location.pathname.substring(0, len) == path) {
+	  element.addClassName('current');
+	}
+};
+
+document.observe('dom:loaded', function() {
+  // Activate nav buttons based on location
+  $('nav').select('a').each(function(element) { 
+  	highlightCurrentLink(element);
+  });
+
+  $('footer').select('a').each(function(element) {
+  	highlightCurrentLink(element);
   });
   
   // Fade out flash boxes
   $$('.fade').each(function(element) {
     new Effect.Fade(element, { delay: 5 });
   });
+  
+  $$('.ajaxForm').each(function(element) {
+ 		
+ 	)};
 });
